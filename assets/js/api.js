5e226dbe072abbfcf190d17b4148a1b968099314
Tiago Marques <tiagomarques2283@hotmail.com>
2025-01-09 21:31:46 -0300

    Atualizando a url do arquivo json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/refs/heads/projeto-base/data/profile.json'
+  const url = 'https://raw.githubusercontent.com/thiiiimarques/Portf-lio_CSS_JavaScript/refs/heads/main/data/profile.json'
   const fetching = await fetch(url);
   return await fetching.json();
 }
