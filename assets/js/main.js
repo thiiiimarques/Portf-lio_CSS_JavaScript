4c963f18bc574ae9e1727f973e28bee15d202686
Tiago Marques <tiagomarques2283@hotmail.com>
2025-01-10 15:08:54 -0300

    Preenchendo as informações do cabeçalho dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,33 @@
 
+
+function updateProfileInfo(profileData){
+  const photo = document.getElementById('profile.photo');
+  photo.src = profileData.photo;
+  photo.alt = profileData.name;
+
+  const name = document.getElementById('profile.name')
+  name.innerText = profileData.name;
+
+  const job = document.getElementById('profile.job')
+  job.innerText = profileData.job
+
+  const location = document.getElementById('profile.location')
+  location.innerText = profileData.location
+
+  const phone = document.getElementById('profile.whatsApp')
+  phone.innerText = profileData.phone
+  phone.href = `tel:${profileData.whatsApp}`
+
+
+  const email = document.getElementById('profile.email')
+  email.innerText = profileData.email
+  email.href = `mailto:${profileData.email}`
+
+}
+
+
+
 (async () => {
-  const profileData = await fetchProfileData()
-  console.log(profileData)
+  const profileData = await fetchProfileData();
+  updateProfileInfo(profileData);
 })()
